72cd511123fa7fb0d7111c5554fa20ec2dd27a75
Charlie Vieth <charlie.vieth@gmail.com>
2015-12-18 19:44:56 -0500

    Comment ev.py

--- gosubl/ev.py
+++ gosubl/ev.py
@@ -3,11 +3,19 @@ import traceback
 
 class Event(object):
     def __init__(self):
-        self.lst = []
         self.lck = threading.Lock()
+
+        # Function list: signature f(*args, **kwargs).  Each function is
+        # called with the arguments to __call__.
+        self.lst = []
+
+        # Function called whenever an item is appended to the Event via
+        # __iadd__.  The signature is post_add(self, f).
         self.post_add = None
 
     def __call__(self, *args, **kwargs):
+        """Called when Event is "called" as a function.
+        """
         with self.lck:
             l = self.lst[:]
 
@@ -20,6 +28,8 @@ class Event(object):
         return self
 
     def __iadd__(self, f):
+        """Override the '+=' operator, appending f to self.lst.
+        """
         with self.lck:
             self.lst.append(f)
 
@@ -32,10 +42,20 @@ class Event(object):
         return self
 
     def __isub__(self, f):
+        """Override the '-=' operator, removing f from self.lst.
+        """
         with self.lck:
             self.lst.remove(f)
 
         return self
 
+# TODO: Figure out how these are used.
+
+# CEV: Called from mg9.py and sh.py.
+#
+# It appears that by setting from handlers (loggers) to Event.lst this can
+# be used for debugging.
 debug = Event()
+
+# CEV: Appears to only be used by GoSublime.py
 init = Event()
