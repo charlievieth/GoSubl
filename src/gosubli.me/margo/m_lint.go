37cbf219b29800f7ca569fd6f59dfce6d8d4138c
Charlie Vieth <cvieth@lyft.com>
2018-06-16 15:54:42 -0400

    WIP: use go instead of python to run compile linter

--- src/gosubli.me/margo/m_lint.go
+++ src/gosubli.me/margo/m_lint.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"go/ast"
 	"go/build"
 	"go/token"
+	"io/ioutil"
 	"os"
+	"os/exec"
 	"regexp"
 	"runtime"
+	"strconv"
 	"strings"
 
+	"github.com/charlievieth/buildutil"
 	"github.com/charlievieth/gotype"
 )
 
@@ -87,6 +93,172 @@ func (m *mLint) Call() (interface{}, string) {
 		"reports": rep,
 	}
 	return res, ""
+}
+
+var (
+	compLintReRowCol  = regexp.MustCompile(`^\d+:\d+:\s+`)
+	compLintReRow     = regexp.MustCompile(`^\d+:\s+`)
+	NoCompileErrors   = []CompLintReport{}
+	ErrCompLintIgnore = errors.New("CompLint: ignore line")
+)
+
+type CompLintReport struct {
+	Row int    `json:"row"`
+	Col int    `json:"col"`
+	Msg string `json:"message"`
+}
+
+type CompLint struct {
+	Filename string
+	Dirname  string
+	Env      map[string]string
+}
+
+func (*CompLint) ParseInt(line []byte) ([]byte, int, error) {
+	i := bytes.IndexByte(line, ':')
+	if i < 1 {
+		return nil, -1, errors.New("invalid line: " + string(line))
+	}
+	n, err := strconv.Atoi(string(line[:i]))
+	if err == nil {
+		line = line[i+1:]
+	}
+	return line, n, err
+}
+
+func (c *CompLint) ParseRowCol(line []byte) (*CompLintReport, error) {
+	line, row, err := c.ParseInt(line)
+	if err != nil {
+		return nil, err
+	}
+	line, col, err := c.ParseInt(line)
+	if err != nil {
+		return nil, err
+	}
+	return &CompLintReport{
+		Row: int(row),
+		Col: int(col),
+		Msg: string(bytes.TrimSpace(line)),
+	}, nil
+}
+
+func (c *CompLint) ParseRow(line []byte) (*CompLintReport, error) {
+	line, row, err := c.ParseInt(line)
+	if err != nil {
+		return nil, err
+	}
+	return &CompLintReport{
+		Row: int(row),
+		Msg: string(bytes.TrimSpace(line)),
+	}, nil
+}
+
+func (c *CompLint) MergeEnv(override map[string]string) []string {
+	a := os.Environ()
+	if len(override) == 0 {
+		return a
+	}
+	seen := make(map[string]bool, len(a)+len(override))
+	env := a[:0]
+	for k, v := range override {
+		if v != "" {
+			seen[k] = true
+			env = append(env, k+"="+v)
+		}
+	}
+	for _, s := range a {
+		if n := strings.IndexByte(s, '='); n != -1 {
+			if k := s[:n]; !seen[k] {
+				seen[k] = true
+				env = append(env, s)
+			}
+		}
+	}
+	// ensure we have a minimal Go environment
+	for k, v := range defaultEnv() {
+		if !seen[k] {
+			env = append(env, k+"="+v)
+		}
+	}
+	return env
+}
+
+func (c *CompLint) CmdArgs() []string {
+	// test
+	if strings.HasSuffix(c.Filename, "_test.go") {
+		return []string{"test", "-c", "-o", os.DevNull}
+	}
+	// main
+	name, err := buildutil.ReadPackageName(c.Filename, nil)
+	if err == nil && name == "main" {
+		return []string{"build", "-o", os.DevNull}
+	}
+	// package
+	return []string{"install"}
+}
+
+// TODO: add cmd to kill list
+func (c *CompLint) Compile() ([]CompLintReport, error) {
+	// TODO: probably don't need this since we set -o to /dev/null
+	dirname, err := ioutil.TempDir("", "margo-")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(dirname)
+
+	// TODO: probably don't need this since we set -o to /dev/null
+	if c.Env == nil {
+		c.Env = make(map[string]string, 1)
+	}
+	c.Env["GOBIN"] = dirname
+
+	var stderr bytes.Buffer
+	cmd := exec.Command("go", c.CmdArgs()...)
+	cmd.Dir = c.Dirname
+	cmd.Env = c.MergeEnv(c.Env)
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err == nil {
+		return NoCompileErrors, nil // no error, nothing to report
+	}
+
+	var rep []CompLintReport
+	var first error
+	check := func(r *CompLintReport, err error) {
+		if err != nil {
+			if first == nil {
+				first = err
+			}
+			return
+		}
+		rep = append(rep, *r)
+	}
+
+	sep := []byte(c.Filename + ":")
+	for _, line := range bytes.Split(stderr.Bytes(), []byte{'\n'}) {
+		line = bytes.TrimSpace(line)
+		if len(line) == 0 || line[0] == '#' {
+			continue
+		}
+		if n := bytes.Index(line, sep); n != -1 {
+			line = line[n+len(sep):]
+			switch {
+			case compLintReRowCol.Match(line):
+				check(c.ParseRowCol(line))
+			case compLintReRow.Match(line):
+				check(c.ParseRow(line))
+			}
+		}
+	}
+	return rep, first
+}
+
+func (c *CompLint) Call() (interface{}, string) {
+	var errStr string
+	rep, err := c.Compile()
+	if err != nil {
+		errStr = err.Error()
+	}
+	return rep, errStr
 }
 
 func init() {
