f05bca596e54bc1930103d69c91abe7d2af715b8
Charlie Vieth <cvieth@lyft.com>
2018-08-18 21:12:37 -0400

    Bump pkgs to b0984a0

--- src/gosubli.me/margo/vendor/github.com/charlievieth/pkgs/pkgs_test.go
+++ src/gosubli.me/margo/vendor/github.com/charlievieth/pkgs/pkgs_test.go
@@ -3,8 +3,30 @@ package pkgs
 import (
 	"go/build"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
+
+const skipDirImportDir = "/go/src/pkg"
+
+var skipDirTests = map[string]bool{
+	"/go/src/other":                 false,
+	"/go/src/pkg":                   false,
+	"/go/src/pkg/vendor":            false,
+	"/go/src/pkg/vendor/foo":        false,
+	"/go/src/pkg/vendor/foo/vendor": true,
+	"/go/src/other/vendor":          true,
+}
+
+func TestSkipDir(t *testing.T) {
+	for path, exp := range skipDirTests {
+		ok := skipDir(skipDirImportDir, path, filepath.Base(path))
+		if ok != exp {
+			t.Errorf("TestSkipDir (%s) expected: %t got: %t", path, exp, ok)
+		}
+	}
+}
 
 func BenchmarkImport(b *testing.B) {
 	pwd, err := os.Getwd()
@@ -18,5 +40,19 @@ func BenchmarkImport(b *testing.B) {
 		if err != nil {
 			b.Fatal(err)
 		}
+	}
+}
+
+const VendorImportPath = "GOPATH/src/github.com/USERNAME/GoSubl/src/gosubli.me/margo/vendor/github.com/charlievieth/gocode/vendor/github.com/golang/groupcache/lru/lru"
+
+func BenchmarkLastVendor(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lastVendor(VendorImportPath)
+	}
+}
+
+func BenchmarkLastVendor_Base(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strings.LastIndex(VendorImportPath, "/vendor/")
 	}
 }
