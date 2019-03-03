15247353f83eb794c194cfdd6250a43e5d92c154
Charlie Vieth <cvieth@lyft.com>
2019-03-03 16:54:59 -0500

    gslint: Remove lint thread since it was unused

--- gslint.py
+++ gslint.py
@@ -4,10 +4,12 @@ import os
 import sublime
 import sublime_plugin
 import threading
-import time
 
 DOMAIN = "GsLint"
 CL_DOMAIN = "GsCompLint"
+
+sem = threading.Semaphore()
+file_refs = {}
 
 
 class FileRef(object):
@@ -24,64 +26,6 @@ class Report(object):
         self.row = row
         self.col = col
         self.msg = msg
-
-
-class GsLintThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.daemon = True
-        self.sem = threading.Semaphore()
-        self.s = set()
-        self.q = gs.queue.Queue()
-
-    def putq(self, fn):
-        with self.sem:
-            if fn in self.s:
-                return False
-            self.s.add(fn)
-            self.q.put(fn)
-            return True
-
-    def popq(self):
-        fn = self.q.get()
-        with self.sem:
-            self.s.discard(fn)
-        return fn
-
-    def run(self):
-        while True:
-            fn = self.popq()
-            fr = ref(fn, False)
-            if fr:
-                reports = {}
-                res, _ = mg9.bcall(
-                    "lint",
-                    {
-                        "dir": (os.path.dirname(fn) if fn else ""),
-                        "fn": fn,
-                        "src": fr.src,
-                        "filter": gs.setting("lint_filter", []),
-                    },
-                )
-                res = gs.dval(res, {})
-                for r in gs.dval(res.get("reports"), []):
-                    if fn and fn != "<stdin>" and r.get("Fn") != fn:
-                        continue
-
-                    kind = r.get("Kind", "")
-                    row = r.get("Row", 0)
-                    col = r.get("Col", 0)
-                    msg = r.get("Message", "")
-                    msg = "%s: %s" % (kind, msg)
-                    if row >= 0 and msg:
-                        reports[row] = Report(row, col, msg)
-
-                fr = ref(fn, False)
-                if fr:
-                    with sem:
-                        fr.state = 1
-                        fr.reports = reports
-                        file_refs[fn] = fr
 
 
 def highlight(fr):
@@ -119,9 +63,8 @@ def highlight(fr):
 
     msg = ""
     reps = fr.reports.copy()
-    l = len(reps)
-    if l > 0:
-        msg = "%s (%d)" % (DOMAIN, l)
+    if len(reps) > 0:
+        msg = "%s (%d)" % (DOMAIN, len(reps))
         r = reps.get(row)
         if r and r.msg:
             msg = "%s: %s" % (msg, r.msg)
@@ -136,66 +79,6 @@ def cleanup(view):
     view.set_status(DOMAIN, "")
     view.erase_regions(DOMAIN)
     view.erase_regions(DOMAIN + "-zero")
-
-
-def watch():
-    global file_refs
-    global th
-
-    view = gs.active_valid_go_view()
-
-    if view is not None and (
-        view.file_name() and gs.setting("comp_lint_enabled") is True
-    ):
-        fn = view.file_name()
-        fr = ref(fn)
-        with sem:
-            if fr:
-                fr.view = view
-                highlight(fr)
-        sublime.set_timeout(watch, 150)  # Previously 500
-        return
-
-    if gs.setting("gslint_enabled") is not True:
-        if view:
-            with sem:
-                for fn in file_refs:
-                    fr = file_refs[fn]
-                    cleanup(fr.view)
-                file_refs = {}
-        sublime.set_timeout(watch, 150)  # Previously 2000
-        return
-
-    if view and not view.is_loading():
-        fn = view.file_name()
-        fr = ref(fn)
-        with sem:
-            if fr:
-                # always use the active view (e.g in split-panes)
-                fr.view = view
-                highlight(fr)
-            else:
-                fr = FileRef(view)
-
-            file_refs[fn] = fr
-            if fr.state == 0:
-                src = view.substr(sublime.Region(0, view.size()))
-                if src != fr.src:
-                    fr.src = src
-                    fr.tm = time.time()
-
-                if fr.tm > 0.0:
-                    timeout = int(gs.setting("gslint_timeout", 500))
-                    delta = int((time.time() - fr.tm) * 1000.0)
-                    if delta >= timeout:
-                        fr.tm = 0.0
-                        fr.state = -1
-                        if not th:
-                            th = GsLintThread()
-                            th.start()
-                        th.putq(fn)
-
-    sublime.set_timeout(watch, 150)  # Previously 500
 
 
 def ref(fn, validate=True):
@@ -259,16 +142,5 @@ class GsCompLintCommand(sublime_plugin.TextCommand):
         fn = self.view.file_name()
         fn = os.path.abspath(fn)
         if fn:
-            dirname = gs.basedir_or_cwd(fn)
             file_refs[fn] = FileRef(self.view)
             mg9.acall("comp_lint", {"filename": fn}, do_comp_lint_callback)
-
-
-try:
-    th
-except:
-    th = None
-    sem = threading.Semaphore()
-    file_refs = {}
-
-    watch()
