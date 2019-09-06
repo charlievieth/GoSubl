0fe317be1f522be3c8f76f123fcb5614249f24f8
Charlie Vieth <cvieth@lyft.com>
2019-09-05 22:54:55 -0400

    bump version

--- gosubl/about.py
+++ gosubl/about.py
@@ -3,8 +3,8 @@ import sublime
 
 # GoSublime Globals
 
-ANN = "a19.04.16-0"
-VERSION = "r18.04.16-0"
+ANN = "a19.04.15-9"
+VERSION = "r18.04.15-10"
 VERSION_PAT = re.compile(r"\d{2}[.]\d{2}[.]\d{2}-\d+", re.IGNORECASE)
 DEFAULT_GO_VERSION = "go?"
 GO_VERSION_OUTPUT_PAT = re.compile(
