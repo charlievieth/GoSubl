2ccd4621c2bc625ae623341109e6a401a05ab9d7
Charlie Vieth <charlie@cockroachlabs.com>
2021-05-07 18:26:46 -0400

    margo: improve logic for finding the project root

--- src/gosubli.me/margo/m_import_paths.go
+++ src/gosubli.me/margo/m_import_paths.go
@@ -140,7 +140,7 @@ func init() {
 
 func isRoot(dir string) bool {
 	// TODO: add ".svn" ".hg" ???
-	for _, name := range []string{"vendor", "go.mod", ".git", "glide.yaml", "Gopkg.toml"} {
+	for _, name := range []string{"go.mod", ".git", "vendor", "glide.yaml", "Gopkg.toml"} {
 		if _, err := os.Lstat(dir + "/" + name); err == nil {
 			return true
 		}
@@ -148,30 +148,110 @@ func isRoot(dir string) bool {
 	return false
 }
 
-func projectRoot(dirname string) string {
-	const sep = string(os.PathListSeparator)
+func hasPathPrefix(s, prefix string) bool {
+	rel, err := filepath.Rel(prefix, s)
+	if err != nil {
+		return false
+	}
+	return !strings.HasPrefix(rel, "..")
+}
 
-	// special case for me
-	for _, path := range strings.Split(build.Default.GOPATH, sep) {
-		pfx := filepath.Join(path, "src", "repl")
-		if strings.HasPrefix(dirname, pfx) {
-			return pfx
-		}
+// shiftPathElements shifts count path elements from path from to path to.
+func shiftPathElements(to, from string, count int) (string, string) {
+	a := strings.Split(filepath.ToSlash(from), "/")
+	if count > len(a) {
+		count = len(a)
+	}
+	to += "/" + strings.Join(a[:count], "/")
+	from = strings.Join(a[count:], "/")
+
+	return filepath.FromSlash(to), filepath.FromSlash(from)
+}
+
+func projectRoot(ctxt *build.Context, dirname string) string {
+	const sep = string(filepath.Separator)
+
+	// WARN
+	if ctxt == nil {
+		ctxt = &build.Default
 	}
 
-	dir := filepath.ToSlash(dirname)
-	for !isRoot(dir) {
-		next := filepath.Dir(dir)
-		if next == dir {
+	root := ctxt.GOROOT + sep + "src"
+	if hasPathPrefix(dirname, root) {
+		return root
+	}
+
+	dirname = filepath.Clean(dirname)
+
+	var subpath string
+	var rootpath string
+	gopaths := strings.Split(ctxt.GOPATH, string(os.PathListSeparator))
+	for _, p := range gopaths {
+		p += sep + "src"
+		s, err := filepath.Rel(p, dirname)
+		if err != nil {
+			continue
+		}
+		if s == "." {
+			// WARN: you cannot have go files at the root of GOPATH
+			// but there's nothing we can do about that here it will
+			// simply result in an error from whatever tool we invoke.
+			return p
+		}
+		if s != "" && !strings.HasPrefix(s, "..") {
+			subpath = s
+			rootpath = p
 			break
 		}
-		dir = next
 	}
-	return dir
+
+	// fmt.Printf("subpath: %q\n", subpath)
+	// fmt.Printf("rootpath: %q\n", rootpath)
+	if subpath == "" {
+		// Search for a project dir outside of the GOPATH (basically, search
+		// without stopping once we've walked outside the GOPATH)
+		//
+		d := dirname
+		for next := filepath.Dir(d); d != next; d, next = next, filepath.Dir(d) {
+			if isRoot(d) {
+				return d
+			}
+		}
+		return d
+	}
+
+	// CEV: special case for me
+	if subpath == "repl" || strings.HasPrefix(subpath, "repl/") ||
+		(sep == "\\" && strings.HasPrefix(subpath, "repl\\")) {
+
+		return filepath.Join(rootpath, "repl")
+	}
+
+	// CEV: since projects cannot live at $GOPATH and are typically at least two
+	// directories removed from the $GOPATH we shift two path elements from the
+	// subdir to the rootdir:
+	//
+	//  ~/go/src ... gh.com/foo/bar
+	// 	 =>
+	//  ~/src/gh.com/foo ... bar
+	//
+	rootpath, subpath = shiftPathElements(rootpath, subpath, 2)
+
+	a := strings.Split(subpath, "/")
+	for i := len(a); i >= 0; i-- {
+		p := rootpath + "/" + strings.Join(a[:i], "/")
+		// fmt.Println("p:", p)
+		if isRoot(p) {
+			return filepath.Clean(p) // WARN: prob don't need clean
+		}
+	}
+
+	return dirname
 }
 
 func importPaths(environ map[string]string, installSuffix, importDir string) ([]string, error) {
-	cacheRoot := projectRoot(importDir)
+
+	cacheRoot := projectRoot(contextFromEnv(environ), importDir)
 	if v, ok := importsPathCache.Load(cacheRoot); ok {
 		if e, _ := v.(*importsPathCacheEntry); e != nil {
 			a := make([]string, len(e.Imports))
