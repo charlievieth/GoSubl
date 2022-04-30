9132539b03e6678614f69755b2a367385e94fa0d
Charlie Vieth <charlie.vieth@gmail.com>
2022-04-29 23:50:06 -0400

    margo: update buildutil to v0.0.7

--- gosubl/about.py
+++ gosubl/about.py
@@ -3,8 +3,8 @@ import sublime
 
 # GoSublime Globals
 
-ANN = "a19.04.15-42"
-VERSION = "r18.04.15-42"
+ANN = "a19.04.15-43"
+VERSION = "r18.04.15-43"
 VERSION_PAT = re.compile(r"\d{2}[.]\d{2}[.]\d{2}-\d+", re.IGNORECASE)
 DEFAULT_GO_VERSION = "go?"
 GO_VERSION_OUTPUT_PAT = re.compile(
